d5868462993047f7bfff5d81c22d63ddc7a9a26c
maom3 <marco.mao@emc.com>
2018-01-29 14:45:24 +0800

    x

--- src/com/marco/rpc/ServiceProxyFactory.java
+++ src/com/marco/rpc/ServiceProxyFactory.java
@@ -1,5 +1,6 @@
 package com.marco.rpc;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
@@ -18,15 +19,42 @@ public class ServiceProxyFactory {
             return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, new InvocationHandler() {
                 @Override
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    Socket socket = new Socket();
-                    socket.connect(new InetSocketAddress("localhost", 8888));
-                    ObjectOutput out = new ObjectOutputStream(socket.getOutputStream());
-                    ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-                    out.writeUTF(clazz.getName());
-                    out.writeUTF(method.getName());
-                    out.writeObject(method.getParameterTypes());
-                    out.writeObject(args);
-                    return in.readObject();
+                    ObjectOutput out = null;
+                    ObjectInputStream in = null;
+                    Socket socket = null;
+                    try {
+                        socket = new Socket();
+                        socket.connect(new InetSocketAddress("localhost", 8888));
+                        out = new ObjectOutputStream(socket.getOutputStream());
+                        in = new ObjectInputStream(socket.getInputStream());
+                        out.writeUTF(clazz.getName());
+                        out.writeUTF(method.getName());
+                        out.writeObject(method.getParameterTypes());
+                        out.writeObject(args);
+                        return in.readObject();
+                    } finally {
+                        if (in != null) {
+                            try {
+                                in.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (out != null) {
+                            try {
+                                out.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (socket != null) {
+                            try {
+                                socket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
             });
         } else {
